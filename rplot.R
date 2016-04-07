005c256bd4ded94bfc1addf6645aa7278ff14838
robinedwards <geotheory1@gmail.com>
2016-04-07 09:53:42 +0100

    Reworked scatterplot, symbols added

--- rplot.R
+++ rplot.R
@@ -1,7 +1,6 @@
-# A small library for rendering bar or scatter plots
-# of csv data to console using ascii characters.
-# (call `Rscript rplot.R -h` for instructions..)
-# by @geotheory | geotheory.co.uk
+# A small library for rendering bar or scatterplots of csv data to console using ascii characters.
+# Call `Rscript rplot.R -h` for instructions..
+# by @geotheory | geotheory.co.uk 2016
 
 # manage input arguments
 args_in = commandArgs(trailingOnly=T)
@@ -10,16 +9,16 @@ args_in = commandArgs(trailingOnly=T)
 pars = list(sep=c('-s',','), pch=c('-p','*'), x=c('-c',50), y=c('-r',20))
 
 # split up combined arguments (e.g. '-am' for aggregate by mean)
-args = unlist(sapply(args_in, function(a){
+args = unlist(sapply(args_in, function(a) {
     if(substr(a,1,1)=='-') return(paste0('-',strsplit(substr(a,2,100),'')[[1]])) else a
   }), use.names = F)
 
 # update pars argument update received
-for(i in 1:length(pars)){
+for(i in 1:length(pars)) {
   p = pars[[i]]
-  if(p[1] %in% args){
+  if(p[1] %in% args) {
     n = match(p[1], args) + 1
-    if(p[1] == '-s'){
+    if(p[1] == '-s') {
       if(is.na(args[n])) args[n] = ""       # whitespace seperator
       if(args[n] == "\\t") args[n] = "\t"   # string to tab char
     }
@@ -31,7 +30,7 @@ for(i in 1:length(pars)){
 plot_args = args[substr(args, 1, 1) == '-']
 field_args = args[!substr(args, 1, 1) == '-']
 
-if(any(c('-h','--help') %in% args_in | '-h' %in% plot_args)){
+if(any(c('-h','--help') %in% args_in | '-h' %in% plot_args)) {
   cat('\n**********************\n')
   cat('******* R-PLOT *******\n')
   cat('**********************\n\n')
@@ -57,26 +56,29 @@ if(any(c('-h','--help') %in% args_in | '-h' %in% plot_args)){
   cat('  Data handling:\n')
   cat('    -n   Specify no header row for input data. Use col indices instead\n')
   cat('    -s   sep character for input data (default `,`). Requires value e.g. ";"  "\\t"  "" (inc. quotes)\n')
-  cat('    -a   Aggregate (default `sum`) hashbar plot data by its categorical variables\n')
-  cat('    -m   Aggregate by `mean` if -a selected\n')
-  cat('    -l   Aggregate by `length` (count instances) if -a selected\n')
+  cat('    -a   Aggregate (default `sum`) a hashbar plot data by its categorical variables\n')
+  cat('    -m   Aggregate by `mean` if `-a` selected\n')
+  cat('    -l   Aggregate by `length` (count instances) if `-a` selected\n')
   cat('  Plotting:\n')
   cat('    -o   Reorder hashbar chart by value (also reorders data.frames)\n')
-  cat('    -H   Override a default scatter plot with hashbar plot\n')
-  cat('    -S   Override a default hashbar plot with scatter plot (NA values are removed)\n')
+  cat('    -H   Override a default scatterplot with hashbar plot\n')
+  cat('    -S   Override a default hashbar plot with scatterplot (NA values are removed)\n')
   cat('    -r   Scatterplot rows/height (default 20). Requires following value.\n')
   cat('    -c   Scatterplot cols/width (default 50). Requires following value.\n')
-  cat('    -p   pch char (defaults `#` and `*`). requires value e.g. -p "." (inc. quotes)\n')
-  cat('    -x   Suppress summary in case of scatter plot\n')
+  cat('    -p   pch char (defaults: `#` hashbars, `*` scatterplots without overplotting,\n')
+  cat('         `. : ■ █` scatterplots with o/p). Requires 1 char eg. `-p "."` (eg. with -y)\n')
+  cat('         or a 4 char string eg. ".°*@" to change overplot symbols (inc. quotes)\n\n')
+  cat('    -x   Suppress summary in case of scatterplot\n')
+  cat('    -y   Suppress scatterplot point symbols (that show overplotting)\n')
   cat('    -z   Suppress plot (eg. use with -P or -Q)\n')
   cat('  Other:\n')
   cat('    -h   Call this help (also --help)\n')
   cat('    -P   Output raw data.frame to console (truncated 1000 rows)\n')
-  cat('    -Q   Output processed data.frame to console (truncated)\n\n')
+  cat('    -Q   Output processed data.frame to console (truncated 1000 rows)\n\n')
   quit()
 }
 
-# functions from scales: included in case R library path isn't accessible from a console call
+# 2 functions from {scales}: included as R's library path sometimes isn't accessible from a console call
 zero_range = function (x, tol = 1000 * .Machine$double.eps) {
   if (length(x) == 1) return(TRUE)
   if (length(x) != 2) stop("x must be length 1 or 2")
@@ -88,70 +90,97 @@ zero_range = function (x, tol = 1000 * .Machine$double.eps) {
   abs((x[1] - x[2])/m) < tol
 }
 
-rescale = function (x, to = c(0, 1), from = range(x, na.rm = TRUE, finite = TRUE)) {
-  if (zero_range(from) || zero_range(to)) return(ifelse(is.na(x), NA, mean(to)))
+rescale = function (x, to=c(0,1), from, finite=T) {
+  if(missing(from)) from = range(x, na.rm=T)
+  if(zero_range(from) || zero_range(to)) return(ifelse(is.na(x), NA, mean(to)))
   (x - from[1])/diff(from) * diff(to) + to[1]
 }
 
-map = function(x, n){
-  x0 = min(0, min(x)); x1 = max(0, max(x))
-  pmin(floor(rescale(x, from=c(x0, x1), to=c(0,n))), n)
-}
+map = function(x, n) floor(rescale(x, to=c(1,n)))
 
-# adapted from http://biostatmatt.com/R/scat.R
-scat <- function(x, y, cols=50, rows=20, pch="*", xlab="x", ylab="Y") {
-  #make an ASCII scatterplot on a rows X cols grid
-  #pch is the ASCII character plotted
-  #check arguments
+scat = function(x, y, cols=50, rows=20, pch="*", xlab="x", ylab="Y") {
   if('-o' %in% plot_args) y = sort(as.numeric(y))
   if(xlab == ylab) xlab = "Index"
   if(missing(x)) x <- 1:length(y)
   else x <- as.numeric(x)
-  if(length(y) != length(x)) stop("lengths of y and x differ")
-  rows <- as.numeric(rows)
-  cols <- as.numeric(cols)
-  if(rows < 1 || cols < 1) stop("rows and cols must be > 1")
-  if(nchar(pch)!=1) stop("pch must be exactly one character")
+  symbs = c('.', ':', '■', '█')
+  if(nchar(pch)==4) symbs = strsplit(pch, '')[[1]]
+  if(nchar(pch)!=1 & nchar(pch)!=4) stop("pch must be 1 or 4 characters long")
 
-  dat = data.frame(x, y, stringsAsFactors=F); names(dat) = c(xlab, ylab)
   # output processed data.frame to console
-  if('-Q' %in% plot_args) print(head(dat,1000))
+  orig_dat = data.frame(x, y, stringsAsFactors=F)
+  names(orig_dat) = c(xlab, ylab)
+  if('-Q' %in% plot_args) print(head(orig_dat,1000))
   if('-z' %in% plot_args) quit()
   
-  #map the y and x values to rows and cols
-  #FIXME values in y or x could be NA or NaN
-  #FIXME division by zero when max(y)-min(y) == 0
-  #FIXME any better way to do this?
+  # rescale to grid and count point overplotting
+  summary = as.data.frame(table(paste(map(x,cols), map(-y,rows))), stringsAsFactors=F)  # summarise
+  summary = data.frame(apply(cbind(do.call('rbind', strsplit(summary[[1]], split=' ')), summary$Freq),2,as.numeric)) # parse
+  names(summary) = c('x','y','freq')
+  op = max(summary$freq)
+  pr_labs = F; labs = NULL
   
-  ymap <- map(y, rows)
-  xmap <- map(x, cols)
-
-  #sort the mapped values so that the are drawn in
-  #left-to-right top-to-bottom order, because thats
-  #how they will be printed, unique because we can
-  #only print one character in a cell
-
-  coords2 = as.data.frame(table(paste(xmap, ymap)), stringsAsFactors=F)
-  coords3 = data.frame(apply(data.frame(cbind(do.call('rbind', strsplit(coords2[[1]], split=' ')), coords2$Freq), stringsAsFactors=F),2,as.numeric))
-  names(coords3) = c('x','y','freq')
-  bitmap = coords3[order(-coords3$y, coords3$x),]
-
-  #print(bitmap)
-  
-  symbs = c('.', ':', '●', '█')
-
-  print(rows)
-  print(cols)
-
-  l = rep(' ', cols)
-  cat(' ', rep('-',cols), ' \n', sep='')
-  for(i in 1:rows){
-    dat = subset(bitmap, y == i)
-    ln = l
-    ln[dat$x] = symbs[dat$freq]
-    cat('|', ln, '|\n',  sep='')
+  if(op > 1 & !'-y' %in% plot_args) {  # overplotting and not manually over-riden   
+    if(nchar(pch)==1 & !'-p' %in% plot_args) warning('Single character argument for -p is ignored when point overplotting is present except when -y selected.')
+    # cluster overplots to map to symbols
+    summary$grp = summary$freq
+    if(length(unique(summary$freq)) > 4) {
+      f = summary$grp != 1   # ignore 1s (over-plotting)
+      centres = unique(as.numeric(quantile(summary$grp[f], 0:2/2)))
+      summary$grp[f] = kmeans(summary$grp[f], centres)$cluster + 1
+    } else{
+      summary$grp = as.numeric(factor(summary$freq))
+    }
+    
+    # symbol labels
+    if(length(unique(summary$freq))) {
+      pr_labs = T
+      # data break points
+      op_data = unique(summary[ order(summary$freq), 3:4 ])
+      op_data_rev = op_data[order(op_data$freq, decreasing=T), ]
+      n = length(unique(op_data$grp))
+      labs = data.frame(p0 = 1:n, p1 = 1:n, lab = '', x=0, stringsAsFactors=F)
+      for(g in unique(op_data$grp)) {
+        labs$p0[g] = op_data$freq[match(g, op_data$grp)]          # first match in group
+        labs$p1[g] = op_data_rev$freq[match(g, op_data_rev$grp)]  # last match in group
+        labs$x[g] = mean(summary$freq[summary$grp == g])          # mean of grp frequencies
+      }
+      for(i in 1:nrow(labs)) {
+        labs$lab[i] = ifelse(labs$p0[i] == labs$p1[i], labs$p0[i], paste0(labs$p0[i], '-', labs$p1[i]))
+        labs$lab[i] = paste0(symbs[i], '  ', labs$lab[i], '  (x̄ ', format(round(labs$x[i],1),nsmall=1), ')')
+      }
+      labs = c('Points', labs$lab)
+    }
+  } else { # only 2 types of 
+    symbs = pch   
+    summary$grp = 1
   }
-  cat(' ', rep('-',cols), ' \n', sep='')
+
+  # output scatterplot
+  l = rep(' ', cols)
+  cat(' ', rep('_',cols), ' \n', sep='')
+  k = 0
+  for(i in 1:rows) {
+    dat = subset(summary, y == i)
+    ln = l
+    ln[dat$x] = symbs[dat$grp]
+    cat('|', ln, '| ',  sep='')
+    if(pr_labs & i <= length(labs)) cat(' ', labs[i], sep='')  # point symbol key
+    k = k + 1
+    if(k == ceiling(rows/2)) cat('', ylab)                     # y label
+    cat('\n')
+  }
+  cat('|', rep('_',cols), '|\n', sep='')
+  xlab_mar = max(0, 1 + cols/2 - (nchar(xlab)/2))
+  cat(rep(' ', xlab_mar), xlab, '\n', sep="")                  # x label
+
+  # add summary unless overriden
+  if(!'-x' %in% plot_args) {
+    cat(nrows, 'data rows plotted')
+    if(nrow(d_orig) > nrows) cat('.', nrow(d_orig) - nrows, 'rows with NA values omitted')
+    cat('\n')
+    print(summary(orig_dat))
+  }
 }
 
 # read in data
@@ -174,17 +203,17 @@ field_names = field_args[2:(length(field_args))]
 num = function(n) !is.na(suppressWarnings(as.numeric(n)))
 
 # interpret field names - check if valid as name or column index
-for(i in length(field_names):1){
+for(i in length(field_names):1) {
   f = field_names[i]
   badfield = F
-  if(!f %in% names(d)){                          # not a valid col name
-    if(num(f)){                                  # is possible number
-      if(as.numeric(f) <= ncol(d)){              # is possible col index range
+  if(!f %in% names(d)) {                          # not a valid col name
+    if(num(f)) {                                  # is possible number
+      if(as.numeric(f) <= ncol(d)) {              # is possible col index range
         field_names[i] = names(d)[as.numeric(f)] # change to col name
       } else badfield = T
     } else badfield = T
   }
-  if(badfield){
+  if(badfield) {
     cat('fieldname not valid name or column index:', f, '\n')
     field_names = field_names[-i]
   }
@@ -197,36 +226,38 @@ values_field = field_names[length(field_names)]
 d = na.omit(d[,c(id_fields, values_field), drop=F])
 nrows = nrow(d) # to calc NA removals
 
-# scatter plot if 2 fully numeric/NA variables or manually specified
-if(length(id_fields) == 1){
+# scatterplot if 2 fully numeric/NA variables or manually specified
+if(length(id_fields) == 1) {
   v = d[[id_fields]]
   numvals = suppressWarnings(as.numeric(v[!is.na(v)]))
   all_numeric = all(!is.na(numvals))
   plot_scatter = F
   if(all_numeric & !'-H' %in% plot_args) plot_scatter = T
   if(!all_numeric & '-S' %in% plot_args) plot_scatter = T
-  if(plot_scatter){
-    if(values_field == id_fields){    # ie. only a single field supplied
+  if(plot_scatter) {
+    if(values_field == id_fields) {    # ie. only a single field supplied
       dat = na.omit(data.frame(x = 1:length(v), y = d[[values_field]], stringsAsFactors=F))
     } else dat = na.omit(data.frame(x = suppressWarnings(as.numeric(v)), y = d[[values_field]], stringsAsFactors=F))
-    scat(dat$x, dat$y, cols=pars$x[2], rows=pars$y[2], pch=pars$pch[2], xlab=id_fields, ylab=values_field)
+    scat(dat$x, dat$y, cols=as.numeric(pars$x[2]), rows=as.numeric(pars$y[2]), pch=pars$pch[2], xlab=id_fields, ylab=values_field)
     quit()
   }
 }
-
 # aggregate
-if('-a' %in% plot_args){
+if('-a' %in% plot_args) {
   fun = 'sum'
   if('-m' %in% plot_args) fun = 'mean'
   if('-l' %in% plot_args) fun = 'length'
   cat('Aggregate function is', fun, '\n')
   if(length(id_fields) > 1) agg_list = as.list(d[,id_fields]) else agg_list = list(d[,id_fields])
-  if(fun == 'length'){
+  if(fun == 'length') {
     d = aggregate(rep(1,nrow(d)), by=agg_list, FUN=sum, na.rm=T, simplify=T)
   } else d = aggregate(d[[values_field]], by=agg_list, FUN=fun, na.rm=T, simplify=T)
   if(id_fields == values_field) values_field = fun # ie. 'length'
 } else{
-  if(length(id_fields) == 1) if(id_fields == values_field) d[[id_fields]] = 1:nrow(d); id_fields = 'Index'
+  if(length(unique(c(id_fields, values_field))) == 1) {
+    d[[id_fields]] = 1:nrow(d)
+    id_fields = 'Index'
+  }
 }
 
 # rename fields if they've changed
@@ -241,19 +272,17 @@ if('-Q' %in% plot_args) print(head(d,1000))
 # calculate column widths
 field_data = list()
 pos_x = 1
-for(f in c(id_fields, values_field)){
+for(f in c(id_fields, values_field)) {
   n = length(field_data) + 1
   maxlen = min(max(nchar(f), nchar(d[[f]])), 30)
   vals = substr(d[[f]], 1, maxlen)
-  padstr = paste0("%-",maxlen,"s")
-  if(f == values_field){
+  padstr = paste0("%-", maxlen, "s")
+  if(f == values_field) {
     numerics = !is.na(suppressWarnings(as.numeric(vals)))
     vals[numerics] = round(as.numeric(vals[numerics]), 2)
   }
-  field_data[n] = list(list(name = sprintf(padstr, substr(f, 1, maxlen)),
-                            values = sprintf(padstr, vals),
-                            pos_start = pos_x,
-                            pos_end = pos_x + maxlen + 2))
+  field_data[n] = list(list(name = sprintf(padstr, substr(f, 1, maxlen)), values = sprintf(padstr, vals),
+                            pos_start = pos_x, pos_end = pos_x + maxlen + 2))
   pos_x = pos_x + maxlen + 3
 }
 
@@ -261,21 +290,21 @@ plot_width = cons_width - field_data[[length(field_data)]]$pos_end
 values = d[[values_field]]
 
 # scale and spacing
-plot_ind = field_data[[length(field_data)]]$pos_end + 2
+plot_ind = field_data[[ length(field_data) ]]$pos_end + 2
 
-# whether scale to zero or negative extreme
-if(min(values) < 0 & max(values) > 0){
+# whether scale to zero or positive/negative extreme
+if(min(values) < 0 & max(values) > 0) {
   ran = range(values)         # scale positive to negative
-} else if(min(values) >= 0){
+} else if(min(values) >= 0) {
   ran = c(0, max(values))     # all positive, scale to zero
 } else{
-  values = -values            # all negative, scale to zero
-  ran = c(0, max(values))     # still plot hashbars from left axis
+  #values = -values           # all negative, scale to zero
+  ran = c(min(values), 0)     # still plot hashbars from left axis
 }
 
-dif = diff(ran)
-fact = (cons_width - plot_ind)/dif
-plot_values = as.integer(rescale(values, to = fact * ran))
+fact = (cons_width - plot_ind) / diff(ran)
+#plot_values = as.integer(rescale(values, to = fact * ran)) # old
+plot_values = as.integer(values * fact)
 minvalue = min(values)
 spaces = pmax(0, pmin(plot_values - fact * minvalue, fact * -minvalue))
 hashes = fact * abs(values)
@@ -287,10 +316,11 @@ if('-p' %in% plot_args) pch = pars$pch[2] else pch = '#'
 if(nrow(d_orig) > nrows) cat('.', nrow(d_orig) - nrows, 'rows with NA values omitted')
 cat('\n'); for(f in field_data) cat(f$name, '  '); cat('\n')
 
-for(i in 1:length(values)){
+for(i in 1:length(values)) {
   # id_fields
   for(f in field_data) cat(f$values[i], '  ')
   # hashes
   cat(rep(' ', spaces[i]), sep='')
   cat(rep(pch, hashes[i]), '\n', sep='')
 }
+
